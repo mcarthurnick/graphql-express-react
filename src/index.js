d6df18d4f854603a145498d04794a1472eb086d8
Nick McArthur <mcarthurnick@gmail.com>
2018-12-23 12:26:01 -0700

    Wrap App in ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloProvider } from 'react-apollo';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import client from './apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+    <ApolloProvider clent={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
+
+if (module.hot) module.hot.accept(); //isn’t really necessary, but makes it so that just the components changing within the app will refresh as you update them, rather than refreshing the entire page
