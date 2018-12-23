b1471bd4e23c3a07427eb82e8015c8e458f38714
Nick McArthur <mcarthurnick@gmail.com>
2018-12-23 12:48:31 -0700

    added submitPost mutation to schema. submitPost either updates the post or creates a new one

--- src/server/index.js
+++ src/server/index.js
@@ -20,6 +20,16 @@ type Post {
     author: String
     body: String
  }
+
+ type Mutation {
+    submitPost(input: PostInput!): Post
+  }
+  
+  input PostInput {
+    id: ID
+    author: String!
+    body: String!
+  }
 `);
 
 const mapPost = (post, id) => post && ({ id, ...post });
@@ -27,6 +37,21 @@ const mapPost = (post, id) => post && ({ id, ...post });
 const root = {
     posts: () => POSTS.map(mapPost),
     post: ({ id }) => mapPost(POSTS[id], id),
+    submitPost: ({ input: { id, author, body } }) => {
+        const post = { author, body };
+        let index = POSTS.length;
+      
+        if (id != null && id >= 0 && id < POSTS.length) {
+          if (POSTS[id].authorId !== authorId) return null;
+      
+          POSTS.splice(id, 1, post);
+          index = id;
+        } else {
+          POSTS.push(post);
+        }
+      
+        return mapPost(post, index);
+      },
 };
 
 const app = express();
